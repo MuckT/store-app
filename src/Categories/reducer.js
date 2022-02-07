1f852e53cdc07318b232403b0f22b724ac286938
MuckT <mcgtom10@gmail.com>
2022-02-06 22:25:18 -0800

    feat: create categories reducer

---
+++ src/Categories/reducer.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  activeCategory: {},
+  categoryList: []
+}
+
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    selectCategory: (state, action) => {
+      state.activeCategory = action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { selectCategory } = counterSlice.actions
+
+export default categoriesSlice.reducer
