db67ca772991ab5bafc74fbb8552f90976a4490a
MuckT <mcgtom10@gmail.com>
2022-02-06 22:23:32 -0800

    test: create mock data

---
+++ src/data/categories.js
@@ -0,0 +1,37 @@
+export const categories = [
+  {
+    name: 'Baby',
+    description: 'Everything your baby needs to giggle with joy.',
+    id: '3eb7875a-9080-4d26-87d8-57f5c2673b90'
+  },
+  {
+    name: 'Toys',
+    description: 'Toys for all ages--everything from dolls and action figures to board games and crafts.',
+    id: '46468ce8-96ac-4716-a38e-0f0842a44eb6'
+  },
+  {
+    name: 'Kids',
+    description: "Kids' apparel, accessories & shoes.",
+    id: 'c251ef69-c381-4ebd-a39d-b70e999c706c'
+  },
+  {
+    name: "Men's Clothing",
+    description: 'Mens apparel, accessories & shoes.',
+    id: '3fa1bdf2-11a6-4719-8a20-467848c305ed'
+  },
+  {
+    name: "Women's Clothing",
+    description: "Women's apparel, accessories & shoes.",
+    id: '31373b63-0a63-464c-a5ca-66f1b739c74c'
+  },
+  {
+    name: 'Electronics',
+    description: 'The latest electronics--from TVs and cell phones to cameras and drones.',
+    id: '03e3e78d-00c1-4399-8155-aa1fe845171e'
+  },
+  {
+    name: 'Garden',
+    description: 'Lawn and garden supplies',
+    id: '17482d46-6f19-49c3-8de4-c22fac778b1d'
+  },
+]
---
+++ src/data/products.js
@@ -0,0 +1,154 @@
+export const products = [
+  {
+    name: 'Mesh Baby Carrier',
+    description: 'Babywearing is now easy and comfortable! With its unique roll-up pocket and cool mesh window for more airflow in warm weather.',
+    price: 49.99,
+    category: 'Baby',
+    id: 'cbddd706-e07e-4c0c-977b-323b8addf53d',
+    inventoryCount: 15
+  },
+  {
+    name: 'Activity Walker',
+    description: 'Activity Walker features multi-directional wheels provide freedom of movement and the extra wide base provides superior support and stability so your baby can be confident as they navigate new terrain.',
+    price: 34.99,
+    category: 'Baby',
+    id: '68f5dc3a-baf1-4c8f-be8d-863371328325',
+    inventoryCount: 1
+  },
+  {
+    name: 'Portable Swing',
+    description: 'Like a monkey swinging from tree to tree, your baby will go bananas for this comfy seat (in the most relaxed way possible). You’ll love knowing you can safely rest your little one in a spot she’ll enjoy ALMOST as much as your arms.',
+    price: 69.99,
+    category: 'Baby',
+    id: '68f5dc3a-baf1-4c8f-be8d-863371328325',
+    inventoryCount: 3
+  },
+  {
+    name: 'Toy Story Woody Figure',
+    description: 'Get ready for exciting Disney Pixar Toy Story 4 adventures with this big variety of characters! Each figure comes in movie-inspired relative scale, with iconic styling.',
+    price: 9.99,
+    category: 'Toys',
+    id: 'a291c68d-c599-4610-a282-c08869bf267a',
+    inventoryCount: 19
+  },
+  {
+    name: 'Batman Action Figure',
+    description: 'This action figure features 11 points of articulation, a cloth cape and detailed comic styling that brings your favorite BATMAN heroes and villains to life!',
+    price: 8.59,
+    category: 'Toys',
+    id: 'c473fa14-57c0-4bcf-b251-3e067cf8cb92',
+    inventoryCount: 15
+  },
+  {
+    name: 'Pokemon Trading Card Game Celebrations Collectors Chest',
+    description: 'This sturdy metal case contains more than a dozen special Pokémon goodies, including many not found anywhere else.',
+    price: 24.99,
+    category: 'Toys',
+    id: 'bbfc0508-da54-4c7b-bc6e-f8e78b9b5354',
+    inventoryCount: 0
+  },
+  {
+    name: 'Hot Wheels Super Ultimate Garage Playset',
+    description: 'Explore the Hot Wheels Super Ultimate Garage for endless storytelling possibilities and opportunities for heroic, save-the-day moments! Standing over 3 feet tall and storing over 140 cars, this is the biggest Hot Wheels playset ever!',
+    price: 224.99,
+    category: 'Toys',
+    id: '132bd67d-03e1-4687-82fe-3b31452ddbdf',
+    inventoryCount: 8
+  },
+  {
+    name: "Men's Straight Fit Jeans",
+    description: 'Give your casual wardrobe a staple foundation with the Straight-Fit Jeans. Woven from breathable cotton with a hint of stretch, these mid-rise jeans are tailored in a straight-fit silhouette, and have five handy pockets accented with signature stitching for a classic look.',
+    price: 20.00,
+    category: "Men's Clothing",
+    id: 'f38c883a-8c91-4da1-a112-c021a2424b81',
+    inventoryCount: 26
+  },
+  {
+    name: "Merino Wool Long Sleeve T-Shirt",
+    description: "Keep comfortable day in and day out with a long-sleeve tee cut in a regular fit that allows for easy wear on its own or layered beneath outerwear. Merino wool lends a soft and warm feel without the bulk, perfect for staying cozy during the cooler months.",
+    price: 24.00,
+    category: "Men's Clothing",
+    id: "814cbf02-4499-4cc4-b446-cebe8cd35e6f",
+    inventoryCount: 37
+  },
+  {
+    name: "Women's Balloon 3/4 Sleeve Eyelet Dress",
+    description: "The Balloon 3/4 Sleeve Eyelet Dress is a great and versatile piece for a grab-and-go outfit for brunch or for a special occasion.",
+    price: 34.99,
+    category: "Women's Clothing",
+    id: "4cc10181-1ca4-4fa2-9578-c466c8252059",
+    inventoryCount: 20
+  },
+  {
+    name: "Women's Cotton Short Sleeve Top",
+    description: "Moisture Wicking Fabric, Quick Drying Fabric",
+    price: 16.00,
+    category: "Women's Clothing",
+    id: "d410180e-a6f0-4a4f-9209-0e58ace5ae31",
+    inventoryCount: 23
+  },
+  {
+    name: "Short Sleeve Notch Collar Top and Shorts Pajama Set",
+    description: "Your day-to-day goal is to wake up feeling restored and rested, ready to take on anything. When going to sleep, you look to relax in sleepwear that’s so serene, drifting off to dreamland will be a breeze.",
+    price: 21.99,
+    category: "Women's Clothing",
+    id: "632372b4-ec65-45b2-8fba-36450982caa4",
+    inventoryCount: 41
+  },
+  {
+    name: "Super-High Rise Distressed Skinny Jeans",
+    description: "Light-wash distressed skinny jeans feature super high-rise design and an ankle length. Made with stretchy material in a tapered, slim-fit cut for comfy wear. Front and back pockets along with front coin pocket lend functional flair.",
+    price: 20.00,
+    category: "Women's Clothing",
+    id: "d14466a8-f1b3-47dc-933f-7d7aea6b3c86",
+    inventoryCount: 36
+  },
+  {
+    name: "Apple AirPods Pro",
+    description: "AirPods Pro feature Active Noise Cancellation for immersive sound. Transparency mode for hearing the world around you. They’re sweat and water resistant¹ and have a customizable fit for all-day comfort.",
+    price: 249.99,
+    category: "Electronics",
+    id: "64128c8e-90cf-40d9-8086-c191777dbfac",
+    inventoryCount: 40
+  },
+  {
+    name: 'TCL 55" Class 4-Series 4K UHD HDR Smart Roku TV',
+    description: "The 4-Series Roku TV delivers stunning 4K picture quality with four times the resolution of Full HD for enhanced clarity and detail, as well as endless entertainment with thousands of streaming channels. High dynamic range (HDR) technology delivers bright and accurate colors for a lifelike viewing experience.",
+    price: 519.99,
+    category: "Electronics",
+    id: "8157ba0f-dd23-4bce-a583-49afcec589d8",
+    inventoryCount: 10
+  },
+  {
+    name: "Total Wireless Prepaid Moxee Mobile Hotspot (256GB)",
+    description: "Do amazing and stay connected anytime anywhere with the Total Wireless Prepaid Mobile Hotspot by Moxee. Connect up to 16 devices on the go with blazing fast speeds. The small, compact device is easy for travel and its full-color screen makes setup and use a breeze.",
+    price: 49.99,
+    category: "Electronics",
+    id: "7f9e9317-d8f2-4889-9173-a85e469ed1d0",
+    inventoryCount: 5
+  },
+  {
+    name: "Apple AirPods (2nd Generation)",
+    description: "AirPods deliver high-quality sound and voice-activated Siri for an unparalleled wireless headphone experience. With the Wireless Charging Case, you can simply place them on a charging mat and go. They’re ready to use with all of your devices. Put them in your ears and they connect immediately. Just like magic.",
+    price: 129.99,
+    category: "Electronics",
+    id: "42cc9647-be4b-4fc6-81c3-a3b731915d05",
+    inventoryCount: 1
+  },
+  { 
+    name: "4PCS Patio Rattan Furniture Set Garden Deck",
+    description: "Are you waiting for a proper rattan chair set to spend the leisure time? Sturdy and solid steel frame with all weather PE rattan can keep stability and durability for long time use. Breathable and soft cushions will hold you comfortably and the cover of cushion can be removed with smooth zipper. Featuring with 1 coffee table, 1 loveseat and 2 single chairs, this patio furniture set can be combined in various ways according to your different needs.",
+    price: 569.99,
+    category: "Garden",
+    id: "e52076f4-422f-478a-8527-f6f20a390ce6",
+    inventoryCount: 8
+  },
+  {
+    name: "Textured Ceramic Planter",
+    description: "Add a hint of artistic craftsmanship to your home with the Textured Ceramic Planter. This rounded freestanding planter features grooved textures that give off a handcrafted appeal you'll love, and the peekaboo stoneware foot features a drainage hole to keep your plants comfortable.",
+    price: 15.00,
+    category: "Garden",
+    id: "b2ccc62d-9ce1-46c5-b3f8-977853f27974",
+    inventoryCount: 12
+  }
+]
