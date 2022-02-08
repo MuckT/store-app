d9c7e7a63b7a1a0705c08dd6e10ac99c31f03d8b
MuckT <mcgtom10@gmail.com>
2022-02-07 21:47:12 -0800

    feat: rework categories to use flatlist and handle orientation changes

--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,30 +1,68 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { FlatList, Image, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { categories } from '../data/categories';
+import { useOrientation } from '../utils/useOrientation';
 import { selectCategory } from './reducer';
 
 const Categories = ({ navigation }) => {
   const dispatch = useDispatch()
   const activeCategory = useSelector(state => state.categories.activeCategory)
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Categories</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-      {categories.map(category => 
-        <Button
-          idx={category.id} 
-          title={activeCategory.id === category.id ? `${category.name} ✅` : category.name}
-          onPress={() => {
-            dispatch(selectCategory(category))
-          }}
-        />
-      )}
+  const orientation = useOrientation()
+  const numColumns = orientation === 'PORTRAIT' ? 2 : 4
+
+  const Item = ({ name, category }) => (
+    <View style={styles.item}>
+      <TouchableOpacity 
+        onPress={() => {
+          dispatch(selectCategory(category))
+        }}
+        
+      >
+        <Text style={activeCategory.id === category.id ? styles.selectedCategory : styles.title}>{name}</Text>
+        <Image source={require('../assets/sample-image.png')}/>
+      </TouchableOpacity>
     </View>
   );
+
+  const renderItem = ({ item }) => (
+    <Item name={item.name} category={item}/>
+  );
+  
+
+  return (
+    <SafeAreaView style={styles.container} scrollIndicatorInsets={{ right: 1 }}>
+      <FlatList
+        data={categories}
+        renderItem={renderItem}
+        key={(numColumns === 2 ? 'h' : 'v')}
+        keyExtractor={item => item.id}
+        numColumns={numColumns}
+        horizontal={false}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+  },
+  selectedCategory: {
+    fontSize: 16,
+    backgroundColor: 'green',
+    color: 'white',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 16,
+    backgroundColor: 'grey',
+    textAlign: 'center',
+  }
+})
 
 export default Categories
